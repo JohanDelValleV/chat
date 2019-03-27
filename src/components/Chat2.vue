b6081709ec9195939bebafb7cf78d88aa3890d69
arturoless <43190646+arturoless@users.noreply.github.com>
2019-03-26 22:53:09 -0600

    ctm

---
+++ src/components/Chat2.vue
@@ -0,0 +1,47 @@
+<template>
+    <v-app>
+        <v-layout>
+            <v-container
+      id="scroll-target"
+      style="max-height: 500px"
+      class="scroll-y"
+    >
+            <v-card-text class="py-0">
+                
+      <v-timeline
+        align-top
+      >
+    <v-timeline-item
+      v-for="n in 4"
+      :key="n"
+      color="red lighten-2" 
+      right
+    >
+      <template v-slot:opposite>
+        <span>Tus eu perfecto</span>
+      </template>
+      <v-card class="elevation-2">
+        <v-card-title class="headline">Lorem ipsum</v-card-title>
+        <v-card-text>
+          Lorem ipsum dolor sit amet, no nam oblique veritus. Commune scaevola imperdiet nec ut, sed euismod convenire principes at. Est et nobis iisque percipit, an vim zril disputando voluptatibus, vix an salutandi sententiae.
+        </v-card-text>
+      </v-card>
+    </v-timeline-item>
+      </v-timeline>
+    </v-card-text>
+    </v-container>
+        </v-layout>
+        <v-layout>
+            <v-btn fab dark color="cyan">
+            <v-icon dark>add</v-icon>
+            </v-btn>
+           <v-textarea
+            height="75px"
+            outline
+            ></v-textarea>
+            <v-btn fab dark color="primary">
+            <v-icon dark>send</v-icon>
+            </v-btn>
+        </v-layout>
+    </v-app>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
 import Vue from 'vue'
 import './plugins/vuetify'
 import App from './App.vue'
+import Vuetify from 'vuetify'
+
+Vue.use(Vuetify, {
+  theme: {
+    primary: '#E53935',
+    secondary:  '#E53935',
+    accent: '#E53935',
+  }
+})
 
 Vue.config.productionTip = false
 
