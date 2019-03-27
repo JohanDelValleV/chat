0199b390fa72892e55de73d0e8946c63fd636e92
WilliamsDJGarcia <43178740+WilliamsDJGarcia@users.noreply.github.com>
2019-03-26 18:43:58 -0600

    Create server.js

---
+++ Servidor/server.js
@@ -0,0 +1,29 @@
+var express = require('express')
+var aplicacion = express()
+
+const net = require('net')
+const server = require('http').Server(aplicacion)
+const socket = require('socket.io')(server)
+const os = require('os')
+
+var HOST = 'redespolitecnica5.ddns.net'
+var PORT =  7000
+
+var ser = net.createServer(function(so){
+    //console.log(so)
+    console.log('Usuario nuevo '+ so.remoteAddress+ ' : ' + so.remotePort )
+
+    so.on('data', function(data){
+        console.log(Buffer.from(data, 'hex').toString('utf8'));
+        so.write('BBCITA');
+    })
+
+    so.on('close', function(){
+        console.log('usuario desconectado')
+    })
+
+})
+
+ser.listen(PORT, HOST);
+
+console.log('Conectado a : ' + HOST + ' : ' + PORT);
