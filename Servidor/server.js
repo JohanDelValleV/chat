9c3fa7cef5215baf34824122932ed57e61807d44
JohanDelValleV <43177845+JohanDelValleV@users.noreply.github.com>
2019-03-31 19:04:05 -0600

    Files with some bugs

--- Servidor/server.js
+++ Servidor/server.js
@@ -1,30 +1,72 @@
-var express = require('express')
-var aplicacion = express()
+"use strict";
+const express = require('express');
+const app = express();
+const http = require('http');
+const httpServer = http.Server(app);
+const io = require('socket.io')(httpServer);
+const SocketIOFile = require('socket.io-file');
+const cors = require('cors');
+// app.get('/', (req, res, next) => {
+// 	return res.sendFile(__dirname + '/client/index.html');
+// });
 
-const fs = require('fs')
-const net = require('net')
-const server = require('http').Server(aplicacion)
-const socket = require('socket.io')(server)
-const os = require('os')
+// app.get('/app.js', (req, res, next) => {
+// 	return res.sendFile(__dirname + '/client/app.js');
+// });
 
-var HOST = 'JOHAN-DELVALLE'
-var PORT =  7000
+// app.get('/socket.io.js', (req, res, next) => {
+// 	return res.sendFile(__dirname + '/node_modules/socket.io-client/dist/socket.io.js');
+// });
 
-var ser = net.createServer(function(so){
-    //console.log(so)
-    console.log('Usuario nuevo '+ so.remoteAddress+ ' : ' + so.remotePort )
+// app.get('/socket.io-file-client.js', (req, res, next) => {
+// 	return res.sendFile(__dirname + '/node_modules/socket.io-file-client/socket.io-file-client.js');
+// });
 
-    so.on('data', function(data){
-        console.log(Buffer.from(data, 'hex').toString('utf8'));
-        so.write('BBCITA');
-    })
+app.use(cors());
 
-    so.on('close', function(){
-        console.log('usuario desconectado')
-    })
+io.origins('*:*')
 
-})
+app.use('/data', express.static('data'));
 
-ser.listen(PORT, HOST);
+io.on('connection', (socket) => {
+	console.log('Socket connected.');
 
-console.log('Conectado a : ' + HOST + ' : ' + PORT);
+	var count = 0;
+	var uploader = new SocketIOFile(socket, {
+		uploadDir: 'data',							// simple directory
+		// accepts: ['audio/mpeg', 'audio/mp3'],		// chrome and some of browsers checking mp3 as 'audio/mp3', not 'audio/mpeg'
+		// maxFileSize: 4194304, 						// 4 MB. default is undefined(no limit)
+		chunkSize: 10240,							// default is 10240(1KB)
+		transmissionDelay: 0,						// delay of each transmission, higher value saves more cpu resources, lower upload speed. default is 0(no delay)
+		overwrite: false, 							// overwrite file if exists, default is true.
+		// rename: function(filename) {
+		// 	var split = filename.split('.');	// split filename by .(extension)
+		// 	var fname = split[0];	// filename without extension
+		// 	var ext = split[1];
+
+		// 	return `${fname}_${count++}.${ext}`;
+		// }
+		// rename: 'MyMusic.mp3'
+	});
+	uploader.on('start', (fileInfo) => {
+		console.log('Start uploading');
+		console.log(fileInfo);
+	});
+	uploader.on('stream', (fileInfo) => {
+		console.log(`${fileInfo.wrote} / ${fileInfo.size} byte(s)`);
+	});
+	uploader.on('complete', (fileInfo) => {
+		console.log('Upload Complete.');
+		console.log(fileInfo);
+	});
+	uploader.on('error', (err) => {
+		console.log('Error!', err);
+	});
+	uploader.on('abort', (fileInfo) => {
+		console.log('Aborted: ', fileInfo);
+	});
+});
+
+httpServer.listen(3030, () => {
+	console.log('Server listening on port 3030');
+});
--- src/components/Chat2.vue
+++ src/components/Chat2.vue
@@ -1,65 +1,94 @@
 <template>
     <v-app>
-        <v-layout>
-            <v-container
-      id="scroll-target"
-      style="max-height: 562.5px"
-      class="scroll-y"
-    >
-            <v-card-text class="py-0">  
-      <v-timeline
-        align-top
+      <v-toolbar dark color="primary">
+          <img width="45px" :src="avatarRemitente">
+         <v-toolbar-title class="white--text">{{remitente}}</v-toolbar-title>
+         <v-spacer></v-spacer>
+      </v-toolbar>
+      <v-snackbar
+      v-model="snackbar"
+        top
+      :timeout="timeout"
+        right
       >
-    <v-timeline-item
-      v-for="n in 2"
-      :key="n"
-      hide-dot
-      color="red lighten-2" 
-      right
-    >
-      <template v-slot:opposite>
-        <span>Tus eu perfecto</span>
-      </template>
-      <v-card class="elevation-2">
-        <v-card-title class="headline">Lorem ipsum</v-card-title>
-        <div class="container">
-          <img alt="" id="imagencita" width="40%" height="40%" @click.stop="dialog = true">
-          <v-dialog 
-          v-model="dialog"
-          width="500">
-          <v-card>
-            <img id="imagencititita" width="100%" height="100%">
-          </v-card>
-            
-          </v-dialog>
-        </div>
-        <div id="viewPdf">
-          <object
-            id="pdf"
-            width="100%"
-            height="100%">
-          </object>
-        </div>
-      </v-card>
-    </v-timeline-item>
-      </v-timeline>
-    </v-card-text>
+        {{ text }}
+        <v-btn
+          color="accent"
+          flat
+          @click="snackbar = false"
+        >
+          Close
+        </v-btn>
+      </v-snackbar>
+        <v-layout>
+          <v-container
+          id="scroll-target"
+          class="scroll-y"
+          >
+          <v-card-text class="py-0" id="timeline">  
+          <v-timeline
+          align-top
+          >
+          <!-- <v-slide-x-transition
+            group
+          > -->
+          <div v-for="msg in messages" :key="msg.time">
+              <div v-if="msg.usuario==usuario">
+              <v-timeline-item
+                right
+              >
+              <template v-slot:icon>
+                <v-avatar>
+                  <img :src="msg.avatar">
+                </v-avatar>
+              </template>
+              <template v-slot:opposite>
+                <span>{{msg.hora}}</span>
+              </template>
+              <v-card id="action" class="elevation-2" width="300px">
+                <v-card-text>{{msg.mensaje}}</v-card-text>
+              </v-card>
+              </v-timeline-item> 
+            </div>
+            <div v-else>
+              <v-timeline-item
+                left
+              >
+                <template v-slot:icon>
+                  <v-avatar>
+                    <img :src="msg.avatar">
+                  </v-avatar>
+                </template>
+                <template v-slot:opposite>
+                  <span>{{msg.hora}}</span>
+                </template>
+                <v-card class="elevation-2" width="300px" dark  color="secondary">
+                  <v-card-text>{{msg.mensaje}}</v-card-text>
+                </v-card>
+              </v-timeline-item> 
+            </div>
+          </div>
+              <!-- </v-slide-x-transition>    -->
+            </v-timeline>
+        </v-card-text>
     </v-container>
-        </v-layout>
+  </v-layout>
         <v-layout>
             <v-btn 
             outline
             fab dark color="primary"
             slot="activator"
             @click="pickFile"
+            
             >
               <v-icon dark>add</v-icon>
-              <input
-              type="file"
-              style="display: none"
-              ref="image"
-              @change="onFilePicked"
-              >
+                <input
+                type="file"
+                style="display: none"
+                ref="image"
+                @change="onFilePicked"
+                id="file"
+                >
             </v-btn>
             <v-textarea
               color="primary"
@@ -79,26 +108,60 @@
 import io from 'socket.io-client';
 import path from 'path';
 import $ from 'jquery';
-import { constants } from 'fs';
-import PDFObject from 'pdfobject';
-const socket = io('157.230.169.186:3000');
-
+import SocketIOFileClient from 'socket.io-file-client';
+const socket = io('http://localhost:3030');
+var uploader = new SocketIOFileClient(socket);
+var auxMessage = '';
 export default {
   data: ()=> ({
+    snackbar: false,
+    timeout: 1000,
+    text: 'Entregado.',
     message: '',
     imageName: '',
-		imageUrl: '',
+  	imageUrl: '',
     imageFile: '',
     img: '',
-    dialog: false
+    usuario: 'Maria',
+    remitente: 'Juan',
+    dialog: false,
+    messages:new Array(),
+    avatarRemitente:'',
+    avatar:'https://avataaars.io/?avatarStyle=Circle&topType=LongHairMiaWallace&accessoriesType=Sunglasses&hairColor=BlondeGolden&facialHairType=Blank&clotheType=BlazerSweater&eyeType=Surprised&eyebrowType=RaisedExcited&mouthType=Smile&skinColor=Pale',
   }),
+  mounted() {
+    socket.on('connect', function () {
+      console.log(':)')
+      
+    });
+    socket.on('maria',(mensaje)=>{
+        this.avatarRemitente=mensaje["avatar"]
+        this.messages.unshift(mensaje)
+        socket.emit('callbackmaria',true)
+    });
+    uploader.on('ready', function() {
+      console.log('SocketIOFile ready to go!');
+    });
+  },
   methods: {
     send(){
-      socket.emit("chat", this.message);
-      this.message = null;
-      socket.on("chat", (mensaje)=>{
-        console.log(mensaje);
-      })
+      if(this.message.trim()!=""){
+        var time = (new Date()).toTimeString().replace(' GMT-0600 (Central Standard Time)','');
+        var mensaje={"usuario":this.usuario,"mensaje":this.message,"hora":time,"posicion":"right","avatar":this.avatar};
+        this.messages.unshift(mensaje)
+        this.messages=this.messages;
+        this.message = null;
+        socket.emit('juan', mensaje);+
+        socket.on('callbackjuan',(callback)=>{
+          if(callback){
+            this.text=time+' entregado.';
+            this.snackbar=true;
+          }
+        })
+      }
+      else{
+        this.message = null;
+      }
     },
     getFileExtension(filename){
       return filename.split('.').pop();
@@ -106,42 +169,76 @@ export default {
     pickFile () {
       this.$refs.image.click ()
     },
-    onFilePicked (e) {
-			const files = e.target.files
-			if(files[0] !== undefined) {
-				this.imageName = files[0].name
-				if(this.imageName.lastIndexOf('.') <= 0) {
-					return
-				}
-				const fr = new FileReader ()
-				fr.readAsDataURL(files[0])
-				fr.addEventListener('load', () => {
-					this.imageUrl = fr.result
-          this.imageFile = files[0] // this is an image file that can be sent to server...
-          socket.emit("sendpath", this.imageFile)
-          socket.on("sendpath", function(info){
-              var image = new Image();
-              var pdf = info.buffer;
-              PDFObject.embed(pdf, '#viewPdf')
-              console.log(pdf)
-              $('#pdf').attr('data', 'data:application/pdf;base64,'+pdf);
-              $('#pdf').attr('src', pdf);
-              image.src = 'data:*;base64,' + info.buffer;
-              $('#imagencita').attr('src',image.src);
-              $('#imagencititita').attr('src',image.src);
-          })
-				})
-			} else {
-				this.imageName = ''
-				this.imageFile = ''
-				this.imageUrl = ''
-			}
+    onFilePicked () {
+          uploader.on('loadstart', function() {
+            console.log('Loading file to browser before sending...');
+          });
+          uploader.on('progress', function(progress) {
+            console.log('Loaded ' + progress.loaded + ' / ' + progress.total);
+          });
+          uploader.on('start', function(fileInfo) {
+            console.log('Start uploading', fileInfo);
+          });
+          uploader.on('stream', function(fileInfo) {
+            console.log('Streaming... sent ' + fileInfo.sent + ' bytes.');
+          });
+          uploader.on('complete', function(fileInfo) {
+            console.log('Upload Complete', fileInfo);
+            document.getElementById('action').addEventListener('click', function(){
+              console.log("data/"+fileInfo.name)
+              var liga = "http://127.0.0.1:3030/data/"+fileInfo.name;
+              var content = '';
+              var request = new XMLHttpRequest();
+              request.open('GET', liga, true);
+              request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+              request.responseType = 'blob';
+            
+              request.onload = function() {
+                // Only handle status code 200
+                if(request.status === 200) {
+                // Try to find out the filename from the content disposition `filename` value
+                var disposition = request.getResponseHeader('content-disposition');
+                var matches = /"([^"]*)"/.exec(disposition);
+                var name = liga.split('http://127.0.0.1:3030/data/')
+                var filename = (matches != null && matches[1] ? matches[1] : name);
+                
+                // The actual download
+                var blob = new Blob([request.response]);
+                var link = document.createElement('a');
+                link.href = window.URL.createObjectURL(blob);
+                link.download = filename;
+            
+                document.body.appendChild(link);
+                link.click();
+                document.body.removeChild(link);
+                }
+                // some error handling should be done here...
+              };
+              request.send('content=' + content);
+            });
+          });
+          uploader.on('error', function(err) {
+            console.log('Error!', err);
+          });
+          uploader.on('abort', function(fileInfo) {
+            console.log('Aborted: ', fileInfo);
+          });
+          auxMessage = "Hola"
+          var fileEl = document.getElementById('file');
+          var uploadIds = uploader.upload(fileEl);
+          let time = (new Date()).toTimeString().replace(' GMT-0600 (Central Standard Time)','');
+          let mensaje ={"usuario":this.usuario,"mensaje":auxMessage,"hora":time,"posicion":"right","avatar":this.avatar};
+          this.messages.unshift(mensaje)
+          this.messages=this.messages
     },
-    
+
   }
 }
 </script>
 
-<style >
-
+<style>
+#scroll-target{
+  height: 500px;
+  width: 100%;
+}
 </style>
