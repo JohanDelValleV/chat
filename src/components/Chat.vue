ed2faea76ff83a43818eeb8e30bbf9d13767bf93
WilliamsDJGarcia <43178740+WilliamsDJGarcia@users.noreply.github.com>
2019-04-04 14:14:48 -0600

    Correction

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -215,9 +215,9 @@ export default {
             console.log('Aborted: ', fileInfo);
           });
           var fileEl = document.getElementById('file');
-          var liga = "http://redespolitecnica5.ddns.net:3030/data/"+fileEl.value.replace('C:\\fakepath\\','');
+          var liga = "http://157.230.166.88:3030/data/"+fileEl.value.replace('C:\\fakepath\\','');
           uploader.upload(fileEl);
-          auxMessage = liga.replace('http://redespolitecnica5.ddns.net:3030/data/','')
+          auxMessage = liga.replace('http://157.230.166.88:3030/data/','')
           let time = (new Date()).toTimeString().replace(' GMT-0600 (Central Standard Time)','');
           let mensaje ={"usuario":this.usuario,"mensaje":auxMessage,"hora":time,"posicion":"right","avatar":this.avatar, "href":liga};
           socket.emit('maria', mensaje);
